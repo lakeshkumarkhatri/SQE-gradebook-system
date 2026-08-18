fb162f99b086f323b24f9d15b4790d0bae3dfd0f
Lakesh Kumar <lakeshkumarkhatri7@gmail.com>
2026-08-18 14:42:34 +0500

    chore: initial project structure

---
+++ src/gradebook/gradebook.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class gradebook {
+    private String name;
+    private String rollNo;
+    private List<Double> scores;
+
+    public gradebook(String name, String rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.scores = new ArrayList<>();
+    }
+}
