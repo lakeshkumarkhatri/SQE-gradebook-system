230af8bac8e90d5037fe06642292baac1ee23cdf
Lakesh Kumar <lakeshkumarkhatri7@gmail.com>
2026-08-29 21:20:28 +0500

    Prevent duplicate roll numbers - Fixes #11

--- src/gradebook/gradebook.java
+++ src/gradebook/gradebook.java
@@ -1,12 +1,22 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class gradebook {
     private String name;
     private String rollNo;
     private List<Double> scores;
 
+    private static Set<String> rollNumbers = new HashSet<>();
+
     public gradebook(String name, String idNumber) {
+        if (rollNumbers.contains(idNumber)) {
+            throw new IllegalArgumentException("Duplicate roll number: " + idNumber);
+        }
+
+        rollNumbers.add(idNumber);
+
         this.name = name;
         this.rollNo = idNumber;
         this.scores = new ArrayList<>();
