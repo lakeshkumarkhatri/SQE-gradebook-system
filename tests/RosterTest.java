ef9f71e0c277d9badcb51ae4536a35472435602e
Lakesh Kumar <lakeshkumarkhatri7@gmail.com>
2026-09-15 14:35:06 +0500

    test: add roster score count boundary tests

--- tests/RosterTest.java
+++ tests/RosterTest.java
@@ -7,7 +7,7 @@ public class RosterTest {
 
     @Test
     void testZeroScoresInvalid() {
-        gradebook student = new gradebook("Student Zero", "ROSTER-0");
+        gradebook student = new gradebook("Student Zero", "BVA-ROSTER-0");
 
         Roster roster = new Roster();
 
@@ -18,12 +18,9 @@ public class RosterTest {
     }
 
     @Test
-    void testThreeScoresValid() {
-        gradebook student = new gradebook("Student Three", "ROSTER-3");
-
-        student.addScore(70);
+    void testOneScoreValid() {
+        gradebook student = new gradebook("Student One", "BVA-ROSTER-1");
         student.addScore(80);
-        student.addScore(90);
 
         Roster roster = new Roster();
         roster.addStudent(student);
@@ -32,10 +29,50 @@ public class RosterTest {
     }
 
     @Test
-    void testEightScoresInvalid() {
-        gradebook student = new gradebook("Student Eight", "ROSTER-8");
+    void testTwoScoresValid() {
+        gradebook student = new gradebook("Student Two", "BVA-ROSTER-2");
+        student.addScore(80);
+        student.addScore(80);
 
-        for (int i = 0; i < 8; i++) {
+        Roster roster = new Roster();
+        roster.addStudent(student);
+
+        assertEquals(1, roster.getStudentCount());
+    }
+
+    @Test
+    void testFiveScoresValid() {
+        gradebook student = new gradebook("Student Five", "BVA-ROSTER-5");
+
+        for (int i = 0; i < 5; i++) {
+            student.addScore(80);
+        }
+
+        Roster roster = new Roster();
+        roster.addStudent(student);
+
+        assertEquals(1, roster.getStudentCount());
+    }
+
+    @Test
+    void testSixScoresValid() {
+        gradebook student = new gradebook("Student Six", "BVA-ROSTER-6");
+
+        for (int i = 0; i < 6; i++) {
+            student.addScore(80);
+        }
+
+        Roster roster = new Roster();
+        roster.addStudent(student);
+
+        assertEquals(1, roster.getStudentCount());
+    }
+
+    @Test
+    void testSevenScoresInvalid() {
+        gradebook student = new gradebook("Student Seven", "BVA-ROSTER-7");
+
+        for (int i = 0; i < 7; i++) {
             student.addScore(80);
         }
 
